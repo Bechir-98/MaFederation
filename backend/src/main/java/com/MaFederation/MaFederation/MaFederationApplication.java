8fb0c52eda4062b92b8e26112a2a5bf49a80fe1b
Bechir <cilebre@hotmail.com>
2025-07-12 15:18:25 +0100

    BackendInit

---
+++ backend/src/main/java/com/MaFederation/MaFederation/MaFederationApplication.java
@@ -0,0 +1,13 @@
+package com.MaFederation.MaFederation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaFederationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaFederationApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/MaFederation/MaFederation/MaFederationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MaFederation.MaFederation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaFederationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
